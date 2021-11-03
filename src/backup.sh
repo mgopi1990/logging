3433075dad09c884cca4cf270a0724e69b2bc901
Gopinath M <mgopi1990@gmail.com>
2021-11-03 22:13:43 +0530

    updated backup directory

--- src/backup.sh
+++ src/backup.sh
@@ -4,6 +4,7 @@ source ./password.sh
 
 DATE=`date +'%d%b%Y'`
 FILENAME="logs_${DATE}.7z"
+HOMEDIR="/home/pi/gopi/gopi_updates/logging/"
 
-cd /home/pi/gopi/gopi_updates/logging/bkp
-7z a $FILENAME -p$PASSWORD ../logs/ ../scripts/
+cd ${HOMEDIR}/bkp/
+7z a $FILENAME -p$PASSWORD ${HOMEDIR}/logs/ ${HOMEDIR}/scripts/.git/ ${HOMEDIR}/scripts/src/ ${HOMEDIR}/scripts/README.md ${HOMEDIR}/scripts/requirements.txt
