920e8bf26cef7ba0db11a111a3238063c2774e4e
Gopinath M <mgopi1990@gmail.com>
2021-11-03 22:54:36 +0530

    added change to delete old backup file

--- src/backup.sh
+++ src/backup.sh
@@ -6,5 +6,13 @@ DATE=`date +'%d%b%Y'`
 FILENAME="logs_${DATE}.7z"
 HOMEDIR="/home/pi/gopi/gopi_updates/logging/"
 
+echo "- BACKUP: $DATE -"
+
 cd ${HOMEDIR}/bkp/
+
+## perform backup
 7z a $FILENAME -p$PASSWORD ${HOMEDIR}/logs/ ${HOMEDIR}/scripts/.git/ ${HOMEDIR}/scripts/src/ ${HOMEDIR}/scripts/README.md ${HOMEDIR}/scripts/requirements.txt
+
+## delete old archieve
+ls -t logs_*.7z | tail -n+6 | xargs rm -vf
+
